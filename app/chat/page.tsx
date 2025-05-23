14e587806967aa7b05de2d598eed9de2c3f935f7
olonts <olonibua0001@gmail.com>
2025-05-23 14:44:48 +0100

    new changes

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,7 +2,12 @@
 import React from 'react';
 import ChatList from '@/components/chat/ChatList';
 
-export default function ChatPage() {
+interface ChatDetailPageProps {
+  params: Promise<{ userId: string }>;
+}
+
+export default async function ChatPage({ params }: ChatDetailPageProps) {
+  const { userId } = await params;
   return (
     <div className="flex-grow overflow-y-auto rounded-tl-3xl border-l border-gray-200 dark:border-gray-800 bg-white">
       <ChatList />
