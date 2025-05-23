7dd206b67010dcf8c3509fa3b22de5b12bfc4390
olonts <olonibua0001@gmail.com>
2025-05-23 14:47:48 +0100

    new changes

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -7,7 +7,7 @@ interface ChatDetailPageProps {
 }
 
 export default async function ChatPage({ params }: ChatDetailPageProps) {
-  const { userId } = await params;
+  // const { userId } = await params;
   return (
     <div className="flex-grow overflow-y-auto rounded-tl-3xl border-l border-gray-200 dark:border-gray-800 bg-white">
       <ChatList />
