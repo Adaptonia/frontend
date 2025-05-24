a9f5c599ea5ec8808b904b986be7fe2e880ce5cc
olonts <olonibua0001@gmail.com>
2025-05-24 09:07:40 +0100

    new

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
-import { signIn } from 'next-auth/react'
 import { toast } from 'sonner'
 import { loginWithGoogle } from '@/src/services/appwrite/auth'
 
