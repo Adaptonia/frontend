38b1674f77556049fb59823d06b2bd316dbdb3db
olonts <olonibua0001@gmail.com>
2025-05-28 23:31:31 +0100

    new changes

--- app/providers.tsx
+++ app/providers.tsx
@@ -13,7 +13,7 @@ export function Providers({ children }: { children: ReactNode }) {
     <AuthProvider>
       <WebSocketProvider>
         <PWAProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} forcedTheme="light">
             {children}
             <Toaster position="bottom-center" />
             <ServiceWorkerRegistration />
